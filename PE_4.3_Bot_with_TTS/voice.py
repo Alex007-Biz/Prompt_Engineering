88b7ca18b84e672d1d9283e26b1fa232d5bb792d
Aleksey <bizukin@yandex.ru>
2025-02-06 08:06:08 +0300

    1

--- PE_4.3_Bot_with_TTS/voice.py
+++ PE_4.3_Bot_with_TTS/voice.py
@@ -1,19 +1,19 @@
-# from elevenlabs import play
-# from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+from elevenlabs.client import ElevenLabs
 from config import elevenlabs_api_key
 
 print(f"config.elevenlabs_api_key: {elevenlabs_api_key}")
 
-# client = ElevenLabs(
-#  api_key=config.elevenlabs_api_key,
-# )
-# print(f"config.elevenlabs_api_key: {config.elevenlabs_api_key}")
-# audio = client.generate(
-#   text="Привет! Меня зовут Дейв.",
-#   voice="Dave",
-#   model="eleven_multilingual_v2"
-# )
-# with open("output.mp3", "wb") as f:
-#     f.write(audio)  # audio — это bytes-объект
-#
-# play(audio)
+client = ElevenLabs(
+ api_key=config.elevenlabs_api_key,
+)
+print(f"config.elevenlabs_api_key: {config.elevenlabs_api_key}")
+audio = client.generate(
+  text="Привет! Меня зовут Дейв.",
+  voice="Dave",
+  model="eleven_multilingual_v2"
+)
+with open("output.mp3", "wb") as f:
+    f.write(audio)  # audio — это bytes-объект
+
+play(audio)
